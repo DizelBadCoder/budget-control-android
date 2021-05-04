187e7eba6ed63231886b2dcf08e3c182ae328782
DizelBadCoder <diskroma69@gmail.com>
2021-05-05 04:39:07 +0800

    add CreateCategoryFragment

---
+++ app/src/main/java/dizel/budget_control/budget_list/view/CreateCategoryFragment.kt
@@ -0,0 +1,20 @@
+package dizel.budget_control.budget_list.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dizel.budget_control.R
+import dizel.budget_control.databinding.FragmentCreateCategoryBinding
+
+class CreateCategoryFragment : Fragment(R.layout.fragment_create_category) {
+
+    private var _binding: FragmentCreateCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentCreateCategoryBinding.bind(view).apply {
+
+        }
+    }
+}
